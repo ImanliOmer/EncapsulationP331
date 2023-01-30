0edb1175b978ac7cd443cca027fd71d28c19a522
ImanliOmer <100156424+ImanliOmer@users.noreply.github.com>
2023-01-30 19:36:38 +0400

    Add files via upload
    alll tasks are uploaded

---
+++ EncapsulationP331/EncapsulationP331/Invoice.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncapsulationP331
+{
+    internal class Invoice
+    {
+        private int account;
+        private string customer;
+        private string provider;
+
+        public Invoice(int account, string customer, string provider)
+        {
+            this.account = account;
+            this.customer = customer;
+            this.provider = provider;
+        }
+
+        private string article;
+
+        public string Article
+        {
+            get 
+            {
+                return article;
+            }
+            set 
+            {
+                article = value;
+            }
+        }
+
+        private int quantity;
+
+        public int Quantity
+        {
+            get
+            {
+                return quantity;
+            }
+
+            set 
+            {
+                quantity = value;
+            }
+        }
+
+        private decimal price;
+
+        public decimal Price 
+        {
+            get 
+            { 
+                return price;
+            }
+
+            set
+            {
+                price = value;
+            }
+        }
+
+
+        public decimal CostCalculation(bool needEdv)
+        {
+            if (needEdv)
+            {
+                return (Price * Quantity) * 0.18m;
+            }
+            else
+            {
+
+                return Price * Quantity;
+            }
+        }
+    }
+
+  
+
+  
+}
---
+++ EncapsulationP331/EncapsulationP331/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace EncapsulationP331
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Invoice invoice = new Invoice(678904, "Alex", "Foxtrot");
+            invoice.Article = "USB-hab";
+            invoice.Quantity = 6;
+            invoice.Price = 30;
+
+            var totalPrice =invoice.CostCalculation(true);
+            Console.WriteLine(totalPrice);
+        }
+    }
+}
